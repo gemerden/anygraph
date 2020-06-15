4a500f6c211b63014bda5fb68d70af731aad0306
gemerden <gemerden@gmail.com>
2020-06-15 11:52:21 +0200

    test fix (due to removed class)

--- anygraph/unittests/test_visitors.py
+++ anygraph/unittests/test_visitors.py
@@ -1,8 +1,7 @@
 import unittest
-import random
 from itertools import product
 
-from anygraph import Many, One, Gather, Iterator, GetEndpoints
+from anygraph import Many, One, Iterator, GetEndpoints
 from anygraph.tools import chained, flipcoin
 
 
@@ -37,24 +36,6 @@ class TestPropagator(unittest.TestCase):
             iterator = Iterator('prevs')
             assert len(list(iterator.iterate(objs[count - 1], breadth_first=breadth_first))) == count
 
-    def test_propagate(self):
-        count = 10
-        objs = self.create_objects(count)
-
-        next_gatherer = Gather('nexts')
-        assert len(next_gatherer(objs[0])) == count
-        assert len(next_gatherer(objs[1])) == count - 1
-
-        prev_gatherer = Gather('prevs')
-        assert len(prev_gatherer(objs[count - 1])) == count
-        assert len(prev_gatherer(objs[count - 2])) == count - 1
-
-        self.back_wire(objs)
-
-        next_gatherer = Gather('nexts')
-        assert len(next_gatherer(objs[count - 1])) == count
-        assert len(next_gatherer(objs[count // 2])) == count
-
     def test_endpoint(self):
         class StartPoint(object):
             next = One('prev')
@@ -65,10 +46,6 @@ class TestPropagator(unittest.TestCase):
         start = StartPoint()
         endpt = EndPoint()
         start.next = endpt
-
-        next_gatherer = Gather('next')
-        nodes = next_gatherer(start)
-        assert len(nodes) == 2
 
         get_endpoints = GetEndpoints('next')
         assert get_endpoints(start) == [endpt]
